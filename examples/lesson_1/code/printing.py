4fc34c3d8a845818d93a62bf2b422e4afacf3a45
Seth Shickluna <35981002+SethShickluna@users.noreply.github.com>
2021-10-02 12:49:52 -0400

    Create printing.py

---
+++ examples/lesson_1/code/printing.py
@@ -0,0 +1,17 @@
+#this is a variable called name, its stores the value "Seth"
+name = "Seth"
+
+#question 1
+print("Question 1", end=": ")
+###############################
+print("Hello", name, sep="")
+
+#question 2 - \n is the newline character, it ends the current line
+print("Question 2", end=": ")
+###############################
+print("Hello", name, sep="\n")
+
+#question 3 - when used with a string the * will multiply that string by some amount
+print("Question 3", end=": ")
+###############################
+print("Hello! ", end =name)
