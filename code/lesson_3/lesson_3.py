a3216142ae287345dcea9dcc21b1fd9e20c77cfa
SethShickluna <sethshicklunapierce@gmail.com>
2021-10-16 18:17:50 -0400

    updated with week 2 homework answers and lesson 3 code!

---
+++ code/lesson_3/lesson_3.py
@@ -0,0 +1,47 @@
+# Lesson_3 code
+
+
+decimal_num = 1.3
+
+# print(type(decimal_num))
+
+string = "Program Over!"
+
+# print(len(string))
+
+if decimal_num < 1.3:
+    decimal_num /= 2
+    # print(decimal_num)
+else:
+    pass
+    # print("It is larger or equal to!")
+
+if 0:
+    pass
+    # print("0 is True!")
+else:
+    pass
+    # print("0 is False!")
+
+
+string = "Python lesson 3"
+
+my_list = [1, 2, 3.14159, 4, 5, 7, 9, 1111]
+
+print(len(my_list))  # what is the output of this print statement?
+
+print(my_list)
+my_list.reverse()
+
+print(my_list)
+
+new_list = [x for x in range(10) if x % 2 != 0]
+
+another_list = [y if y > 5 else -1 for y in my_list]
+
+print(another_list)
+
+
+
+
+
